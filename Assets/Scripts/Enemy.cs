3994f4e29d311b7fa3a686a23c63a2bb58741907
Andy Daytona <zfesandydaytona@gmail.com>
2021-10-11 00:03:51 +0300

    [Edhen-Assault] enemy hitpoints logic implemented

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,6 +9,7 @@ public class Enemy : MonoBehaviour
     [Tooltip("FX prefab on enemy")][SerializeField] GameObject deathFX;
     [Tooltip("Object to spawn runtime")][SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 12;
+    [SerializeField] int hits = 10;
 
     ScoreBoard scoreBoard;
     void Start()
@@ -26,9 +27,23 @@ public class Enemy : MonoBehaviour
     void OnParticleCollision(GameObject other)
     {
         //print("Particles collided with enemy" + gameObject.name);
+        ProcessHit();
 
+        if (hits <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
         scoreBoard.AddScoreHit(scorePerHit);
 
+        hits--;
+    }
+
+    private void KillEnemy()
+    {
         // Quaternion.identity is a neutral rotation (we dont change it);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
